ec68ff6f2cc00000d6b5ae1307ab5e3d4d7a846c
EuYohas <yohascherer@gmail.com>
2020-10-14 22:03:25 -0300

    olha prud

--- node/scan_test.py
+++ node/scan_test.py
@@ -9,10 +9,39 @@ Created on Thu Oct  8 16:14:52 2020
 import rospy
 from sensor_msgs.msg import LaserScan
 
+
+def corrigeGap(lista, valorMin, x=3):
+    """
+    Entradas é a lista e o valorMinimo 
+    Retorna a lista transformando todos os valores menores do que o valor minimo em 0
+    Ordem de grandeza máximo 8n -> O(n)
+    """
+    newlista= []
+    for c in range(len(lista)):
+        if lista[c] <= valorMin:
+            newlista.append(c)
+    for n in newlista:
+        for i in range(7):
+            a=(n + x - i)
+            if   a >= 0 and a < len(lista):
+                lista[a] = 0
+                
+    #print str(lista) #print para testes
+    return lista
+
 def callback(msg):
     t=len(msg.ranges)
+    minDist=3.0
+    tempList=[round(msg.ranges[i], 3) for i in range(t) if (i>rangeMin and i<rangeMax)] #transformar a tupple de valores de msgs.ranges
     rospy.loginfo ("O numero de valores lidos são" + str(t))
-
+    #dist = corrigeGap(tempList, minDist)
+    dist=tempList
+    rospy.loginfo(str(dist))
+    
+    
 rospy.init_node("scan_values")
+rangeMax=int(260*1080/360)
+rangeMin=int(100*1080/360)
 sub = rospy.Subscriber('/scan', LaserScan, callback)
-rospy.spin()
+rate=rospy.Rate(10)
+rate.sleep()
