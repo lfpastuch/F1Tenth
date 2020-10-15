ffe0a94cc43330e26571c4593f278dff4b13273e
EuYohas <yohascherer@gmail.com>
2020-10-15 17:11:09 -0300

    follow the gap pronto

--- node/GapFollow.py
+++ node/GapFollow.py
@@ -7,11 +7,13 @@ Created on Sun Oct 11 17:39:38 2020
 """
 #Importando as bibliotécas e as mensagens que serão usadas
 import rospy
+import time
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
-
-gapArray = [] #definindo a variavel global que será compartilhada, assim 
+#
+gapArray = [0] #definindo a variavel global que será compartilhada, assim 
 bestGapTu = ()
+
 
 def corrigeGap(lista, valorMin, x = 3):
     """
@@ -43,47 +45,112 @@ def callback(msg):
     #define-se o arco de visão onde estamos procurando os obstáculos, e a menor distância entre o carro e os obstáculos.
     rangeMax=int(270*1080/360)  
     rangeMin=int(90*1080/360)
-    minDist=2.0
+    #minDist=4.0
 
     #transformar em lista a tupple de valores de msgs.ranges adquiridas do Lidar
-    t=len(msg.ranges)
-    tempList=[round(msg.ranges[i], 3) for i in range(t) if (i>rangeMin and i<rangeMax)] 
+#    t=len(msg.ranges)
+#    print "\n"
+#    inicio = time.time()
+#    tempList=[msg.ranges[i] for i in range(t) if (i>rangeMin and i<rangeMax)] 
+#    fim = time.time()
+#    print "for "+str(fim - inicio)
     
-    #função para retornar a lista de leituras com as lacunas sendo valores diferentes de zero
-    dist = corrigeGap(tempList, minDist)
+    #transformar em lista a tupple de valores de msgs.ranges adquiridas do Lidar
+    inicio = time.time()
+    tempList=list(msg.ranges[rangeMin:rangeMax])
+    fim = time.time()
+    print "list "+str(fim - inicio)
     
-    #rospy.loginfo(str(dist)) #usado para teste
+    inicio = time.time()
     
-    #atualiza o gapArray com o melhor
-    gapArray=dist[:]
-    
-    direc_msg = AckermannDriveStamped()
-    direc_msg.drive.steering_angle = 0
-    direc_msg.drive.speed = 0.1
-    
-    global gap_pub
-    
-    gapTuple=melhorGap(gapArray)
-    
-    mod = (gapTuple[0]-int(gapTuple[3]/2))*0.005823
-    if mod>0.4:
-        direc_msg.drive.steering_angle = 0.4
-    elif mod<-0.4:
-        direc_msg.drive.steering_angle = -0.4
-    else:
-        direc_msg.drive.steering_angle = (gapTuple[0]-int(gapTuple[3]/2))*0.005823
-    
-    # Aqui eh declarada a mensagem que fara o carro virar para algum dos lados.
-	
-	print str(gapTuple)
-    if(msg.ranges[540] > 11):	
-        direc_msg.drive.speed = 5
-    else:
-        direc_msg.drive.speed = 0.1+0.5*msg.ranges[540]
+    #atualiza o gapArray com a leitura atual
+    gapArray=tempList[:]
+#    
+#    print str(msg.ranges[540])
+#    #print(tempList)
+#    
+#    #função para retornar a lista de leituras com as lacunas sendo valores diferentes de zero
+#    gapArray = corrigeGap(tempList, minDist)
+#    fim = time.time()
+#    print "CorrigeGap "+str(fim - inicio)
+#    
+#    inicio = time.time()
+#    #rospy.loginfo(str(dist)) #usado para teste
+#    
+#    
+##    gapArray=dist[:]
+#    
+#    
+#    
+#    direc_msg = AckermannDriveStamped()
+#    direc_msg.drive.steering_angle = 0
+#    direc_msg.drive.speed = 0.5
+#    
+#    global gap_pub
+#    
+#    gapTuple=melhorGap(gapArray)
+#    fim = time.time()
+#    print "melhorGap "+str(fim - inicio)
+#    
+#    
+#    direc_msg.drive.steering_angle = ((gapTuple[0]-int(gapTuple[3]/2))*0.4/(gapTuple[3]/2))
+##    mod = (gapTuple[0]-int(gapTuple[3]/2))*0.005823
+##    if mod>0.4:
+##        direc_msg.drive.steering_angle = 0.4
+##    elif mod<-0.4:
+##        direc_msg.drive.steering_angle = -0.4
+##    else:
+##        direc_msg.drive.steering_angle = ((gapTuple[0]-int(gapTuple[3]/2))*0.005823)
+#    
+#    # Aqui eh declarada a mensagem que fara o carro virar para algum dos lados.
+#	
+#	#
+##    
+##    if(msg.ranges[540] > 11):	
+##        direc_msg.drive.speed = 5
+##    else:
+##        direc_msg.drive.speed = 0.1+0.5*msg.ranges[540]
+#
+#    gap_pub.publish(direc_msg)
+#    
+#    #print str(gapArray)
+#    print str(gapTuple)
 
-    #gap_pub.publish(direc_msg)
-    
-    
+#def melhorGap2(listaGaps):
+#    """
+#    Entrada é uma lista de valores de lacunas e zeros. 
+#    Retornar um tuple com o primeiro valor sendo o index central do melhor lacuna de não zeros dessa lista, 
+#    o segundo valor sendo o numero de valores tem essa lacuna, o terceiro o valor da média desses numeros, e o 
+#    quarto o numero de valores que tem a lista.
+#    Ordem de grandeza -> O(n²)
+#    """
+#    
+#    # print str(listaGaps)
+#    melhorTupla=(0, 0, 0, 0)
+#    tamLista=len(listaGaps)
+#    listaNova=[]
+#    med = 0
+#    centro=0
+#    tamLac=0
+#    listaGaps.append(0)
+#    for v in range(len(listaGaps)):
+#        if listaGaps[v] == 0:
+#            if len(listaNova) > 0:
+#                tamLac=len(listaNova)
+#                med=sum(listaNova)/len(listaNova)
+#                centro=v+int(len(listaNova)/2)
+#                if tamLac>melhorTupla[1] or (tamLac==melhorTupla[1] and med>melhorTupla[2]):
+#                    melhorTupla=(centro, tamLac, med, tamLista)
+#                listaNova=[]
+#                
+#        else:
+#            listaNova.append(listaGaps[v])
+#            
+#    
+#    #print str(melhorTupla)     #usado para teste      
+#    return melhorTupla
+
+
 def melhorGap(listaGaps):
     """
     Entrada é uma lista de valores de lacunas e zeros. 
@@ -93,6 +160,7 @@ def melhorGap(listaGaps):
     Ordem de grandeza -> O(n²)
     """
     
+   # print str(listaGaps)
     melhorTupla=(0, 0, 0, 0)
     tamLista=len(listaGaps)
     for v in range(len(listaGaps)):
@@ -127,20 +195,17 @@ class FollowTheGap(object):
         self.sub = rospy.Subscriber('/scan', LaserScan, callback)
 
 
-        drive_msg = AckermannDriveStamped()
-        drive_msg.drive.steering_angle = 0
-        drive_msg.drive.speed = 1
-		
-        rate = rospy.Rate(10)
+
+        rate = rospy.Rate(100)
 		
         while not rospy.is_shutdown():
-            global drive_pub
-            #gap_pub.publish(drive_msg)
+            global gapArray
+            print gapArray
+            self.GapFollow(gapArray)
             rate.sleep()    
             
             
             
-#           
 #           gapTuple=melhorGap(gapArray)
 #           #direc_msg.drive.steering_angle = 0
 #
@@ -164,17 +229,42 @@ class FollowTheGap(object):
 #            print str(gapTuple)
 #            rate.sleep()
 #            
+    def GapFollow(self, listaScan):
 
-    def shutdown(self):
-        global direc_msg
-        rospy.loginfo("Parada")
-
-        shut_msg = AckermannDriveStamped()
-        shut_msg.drive.speed = 0
-        shut_msg.drive.steering_angle = 0
-        #gap_pub.publish(shut_msg)
-
-        rospy.sleep(1)
+        minDist = 3.0
+        #função para retornar a lista de leituras com as lacunas sendo valores diferentes de zero
+        GPArray=corrigeGap(listaScan, minDist)
+        #rospy.loginfo(str(dist)) #usado para teste
+        direc_msg = AckermannDriveStamped()
+        direc_msg.drive.steering_angle = 0
+        
+        
+        direc_msg.drive.speed = 0.5
+    
+        global gap_pub
+    
+        gapTuple=melhorGap(GPArray)
+        if not gapTuple == (0,0,0,0):
+            direc_msg.drive.steering_angle = ((gapTuple[0]-int(gapTuple[3]/2))*0.4/(gapTuple[3]/2))
+            
+            if GPArray[int(gapTuple[3]/2)] > 5:
+                direc_msg.drive.speed = 2
+            else:
+                direc_msg.drive.speed = 0.5
+            gap_pub.publish(direc_msg)
+#        
+            
+        
+#    def shutdown(self):
+#        global direc_msg
+#        rospy.loginfo("Parada")
+#
+#        shut_msg = AckermannDriveStamped()
+#        shut_msg.drive.speed = 0
+#        shut_msg.drive.steering_angle = 0
+#        gap_pub.publish(shut_msg)
+#
+#        rospy.sleep(1)
 def main():
 
     fg = FollowTheGap()
